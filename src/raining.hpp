7cea49efde9327e73a86d00736d1a3b36dd51b2d
jhashilkar <jhashilkar3@mail.com>
2026-02-25 19:05:44 -0600

    Final commit

--- src/raining.hpp
+++ src/raining.hpp
@@ -1,5 +1,10 @@
-#pragma once
+#ifndef RAINING_HPP
+#define RAINING_HPP
 
 #include <string>
+#include <vector>
 
-std::string hello();
+void addAndSort(std::vector<std::string>& businesses, const std::string& name);
+void printBusinesses(const std::vector<std::string>& businesses);
+
+#endif
